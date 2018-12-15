6c6d6c472b5d29f500d3716daa7a74e580673d49
franco <franco.cazzaro@unique.it>
2018-12-15 17:13:46 +0100

    Salvataggio nel DB ok

--- src/vppnHandler.php
+++ src/vppnHandler.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\vppn;
+
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\node\NodeInterface;
 
@@ -15,45 +16,66 @@ class vppnHandler {
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
+
   /**
    * Constructs a new vppnHandler object.
    */
   public function __construct(EntityTypeManagerInterface $entity_type_manager) {
     $this->entityTypeManager = $entity_type_manager;
+
+
   }
 
-  public function getDefaultsForNode($form){
+  public function getDefaultsForNode($form) {
     return [];
   }
 
-  public function checkIfContentTypeEnabled(){
-    if(!\Drupal::currentUser()->hasPermission('use vppn')){
+  public function checkIfContentTypeEnabled() {
+    if (!\Drupal::currentUser()->hasPermission('use vppn')) {
       return FALSE;
     }
     /** @var \Drupal\node\Entity\NodeType $nodeType */
     $routeName = \Drupal::routeMatch()->getRouteName();
-    if($routeName == 'node.add') {
+    if ($routeName == 'node.add') {
       $nodeType = \Drupal::routeMatch()->getParameters()->get('node_type');
       $nodeType = $nodeType->get('type');
       $config = \Drupal::config('vppn.vppnconfig')->get('vppn_node_list');
-    } elseif ($routeName == 'entity.node.edit_form'){
+    }
+    elseif ($routeName == 'entity.node.edit_form') {
       /** @var NodeInterface $nodeType */
       $nodeType = \Drupal::routeMatch()->getParameters()->get('node');
       $nodeType = $nodeType->getType();
       $config = \Drupal::config('vppn.vppnconfig')->get('vppn_node_list');
     }
-    if(is_null($config)){
+    if (is_null($config)) {
       return FALSE;
     }
-    return in_array($nodeType,$config,TRUE);
+    return in_array($nodeType, $config, TRUE);
   }
 
-  public function cleanEntriesByEntityId($id){
+  public function cleanEntriesByEntityId($id) {
     //pulisce tutte le tuple relative alla tabella
+
+
+    $connection = \Drupal::database();
+
+    $connection->delete('vppn')
+      ->condition('nid', $id)
+      ->execute();;
+
   }
 
-  public function insertRoleEntry($roleName,$value){
+
+  public function insertRoleEntry($nid, $rid) {
     //aggiorna
-  }
 
+
+    $connection = \Drupal::database();
+    $connection->insert('vppn')
+      ->fields([
+        'nid' => $nid,
+        'rid' => $rid,
+      ])
+      ->execute();
+  }
 }
